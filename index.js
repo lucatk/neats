aed83f206e80840e2c5d8e65f0023b8935237c43
lucatk <lucakillmaier@lu-co.de>
2017-08-10 01:03:07 +0200

    Removed pipe backend, use librespot default

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { spawn } = require("child_process");
 const Speaker = require("speaker");
 const microtime = require("microtime");
+const fs = require('fs');
+
+"cache/librespot".split("/").reduce((path, folder) => {
+  path += folder + "/";
+  fs.mkdirSync(path);
+});
 
 const speakerOptions = {
   channels: 2,
@@ -11,8 +17,7 @@ var speaker = new Speaker(speakerOptions);
 
 var librespot = spawn("ext/librespot/librespot", [
   "--cache", "cache/librespot",
-  "--name", "neats",
-  "--backend", "pipe"
+  "--name", "neats-test"
 ]);
 
 var timeout = null;
